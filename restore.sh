0b278a2986f6d813ffba4c268fe9d3486bda7ca7
Nicolas Challeil <nicolas.challeil@openclassrooms.com>
2022-04-11 19:00:48 +0200

    prompt user and show diff before copying files

--- restore.sh
+++ restore.sh
@@ -2,4 +2,34 @@
 
 DESTINATION_DIR=$1
 
-cp -ri ./config/* $DESTINATION_DIR
+DIFFED_COUNTER=0
+
+for FILE in $(find . -type f -path './config/**'); do
+    DESTFILE=${FILE/.\/config/$DESTINATION_DIR}
+    if test -f "$DESTFILE"; then
+        DIFF="$(diff $DESTFILE $FILE)"
+
+        if [[ ! -z "$DIFF" ]]; then
+            DIFFED_COUNTER=$((DIFFED_COUNTER + 1))
+            diff -u --color $DESTFILE $FILE
+            while true; do
+                read -r -p "Do you wish to overwrite $DESTFILE? [Y/n] " input
+
+                case $input in
+                [yY][eE][sS] | [yY] | "")
+                    cp -f $FILE $DESTFILE
+                    break
+                    ;;
+                [nN][oO] | [nN])
+                    break
+                    ;;
+                esac
+            done
+
+        fi
+    fi
+done
+
+if [ "$DIFFED_COUNTER" = 0 ]; then
+    echo "No differences, no copied files."
+fi
