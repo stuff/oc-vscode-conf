d58534a8c08df4733bec7837c08c666ac4a0773c
Nicolas Challeil <nicolas.challeil@openclassrooms.com>
2022-04-11 18:39:53 +0200

    mac compatible get-files command

--- get-files.sh
+++ get-files.sh
@@ -4,6 +4,8 @@ SOURCE_DIR=$1
 DESTINATION_DIR=$2
 
 cd $SOURCE_DIR
-find . -path '*/**/.vscode' -not -path '*/node_modules/*' -exec cp --parents -r {} $DESTINATION_DIR \;
+
+find . -path '*/**/.vscode/***' -not -path '*/node_modules/*' -print0 | rsync -aR --files-from=- --from0 ./ $DESTINATION_DIR
+
 cp openclassrooms.code-workspace $DESTINATION_DIR
 cd -
